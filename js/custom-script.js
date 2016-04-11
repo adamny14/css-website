da1b51ff1fd8bc142f9ce2d779b2674f7cb9b89c
Taylor Ninesling <t9sling@hotmail.com>
2016-04-11 14:17:43 -0400

    Revert master branch to start from scratch (old version can be found on example branch)

--- js/custom-script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  // Project Information/Descriptions
-  $(".proj").hover(function(){
-    $("img", this).fadeToggle(500);
-    $(".proj-description", this).fadeToggle(500);
-  });
-});
