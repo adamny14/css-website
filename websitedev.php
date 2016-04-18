95640969a9bbc50c466d3284516a7b11b123362f
Adam Hussain <adamahussain528@yahoo.com>
2016-04-18 14:00:43 -0400

    Added Bootstrap
    Added bootstrap to the webpage

--- websitedev.php
+++ websitedev.php
@@ -1,1 +1,13 @@
-<Doctype html>
+<!Doctype html>
+<html>
+<head>
+    <title>Website Devlopment </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="catalog.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </head
+
+
+</html>
