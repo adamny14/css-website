003f3b01085a7f3fa7c874b0544e2233e5680aef
Adam Hussain <adamahussain528@yahoo.com>
2016-04-18 14:18:01 -0400

    Added A title
    Added a title and words to the webpage

--- websitedev.php
+++ websitedev.php
@@ -1,13 +1,32 @@
-<!Doctype html>
+<!DOCTYPE html>
 <html>
-<head>
+  <head>
     <title>Website Devlopment </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="catalog.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-  </head
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="page-header">
+        <h1 class="centered-text">CSS Website Development </h1>
+
+        </div>
+      </div>
+
+
+
+      <div class="row">
+        <div class ="col-sm-2">
+          <p> Hello it's me </p>
+        </div>
+        <div class="col-xs-10">
+        </div>
+      </div>
+  </body>
+
 
 
 </html>
